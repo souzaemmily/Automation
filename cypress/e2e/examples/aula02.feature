79b21aa2bb125ea63f0d362b099b4d0dcd8b3eb3
unknown <emmily.souza@acct.global>
2023-05-10 17:49:55 -0300

    Aula 02

---
+++ cypress/e2e/examples/aula02.feature
@@ -0,0 +1,8 @@
+Feature: Form Test - Aula02
+
+    Scenario: Login User with incorrect email and password
+        
+        Given I access the AutomationExercise 
+        When I click on Login button
+        And I fill all input fields
+        Then I click on login button
--- cypress/support/step_definitions/steps.js
+++ cypress/support/step_definitions/steps.js
@@ -8,3 +8,19 @@ When("I search for Grupo Quality", () => {
     cy.get('[title="Pesquisar"]').type('Quality Digital{enter}')
 })
 
+//AULA 02
+
+Given("I access the AutomationExercise", () => {
+    cy.visit("https://automationexercise.com")
+})
+
+When("I click on Login button", () => {
+    cy.get('.shop-menu > .nav > :nth-child(4)').click()
+})
+And("I fill all input fields", () => {
+    cy.get('[data-qa="login-email"]').type("emmily.souza@qualitydigital.global")
+    cy.get('[data-qa="login-password"]').type("@ccT1234")
+})
+Then("I click on login button", () => {
+    cy.get('[data-qa="login-button"]').click()
+})
